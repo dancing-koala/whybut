cf9b2fd0baddeeb657a9d425513f8814d8fb86ba
Corentin Bruneau <c.bruneau@ubitransports.com>
2017-07-29 19:41:52 +0200

    Refactor mvp elements

--- app/src/main/java/com/dancing_koala/whathaveyoubeenupto/settings/mvp/SettingsPresenter.java
+++ app/src/main/java/com/dancing_koala/whathaveyoubeenupto/settings/mvp/SettingsPresenter.java
@@ -1,5 +1,6 @@
 package com.dancing_koala.whathaveyoubeenupto.settings.mvp;
 
+import com.dancing_koala.whathaveyoubeenupto.data.repository.EntryRepository;
 import com.dancing_koala.whathaveyoubeenupto.settings.SettingsManager;
 
 /**
@@ -8,89 +9,47 @@ import com.dancing_koala.whathaveyoubeenupto.settings.SettingsManager;
 
 public class SettingsPresenter implements ISettingsPresenter {
 
-    private boolean[] mDaysOfWeekStates;
     private SettingsManager mSettingsManager;
-    private ISettingsEditorView mSettingsEditorView;
+    private ISettingsView mSettingsView;
+    private EntryRepository mEntryRepository;
 
-    private boolean mReminder1State;
-    private String mReminder1Time;
+    private int mCurrentEntryLifespan;
 
-    private boolean mReminder2State;
-    private String mReminder2Time;
-
-    private boolean mReminder3State;
-    private String mReminder3Time;
-
-    public SettingsPresenter(SettingsManager settingsManager) {
+    public SettingsPresenter(SettingsManager settingsManager, EntryRepository entryRepository) {
         mSettingsManager = settingsManager;
-
-        mDaysOfWeekStates = mSettingsManager.getWeekDays();
+        mEntryRepository = entryRepository;
+        mCurrentEntryLifespan = settingsManager.getEntryLifespan();
     }
 
     @Override
-    public void attachView(ISettingsEditorView view) {
-        mSettingsEditorView = view;
-        mSettingsEditorView.displayDaysOfWeekStates(mDaysOfWeekStates);
+    public void attachView(ISettingsView view) {
+        mSettingsView = view;
+        mSettingsView.displayCurrentEntryLifespan(mCurrentEntryLifespan);
     }
 
     @Override
     public void detachView() {
-        mSettingsEditorView = null;
+        mSettingsView = null;
     }
 
     @Override
-    public void setDayOfWeekState(int index, boolean state) {
-        if (index < 0 || index >= mDaysOfWeekStates.length) {
-            return;
+    public void onDeleteAllEntriesClicked() {
+        if (mSettingsView != null) {
+            mSettingsView.displayDeleteAllEntriesConfirmation();
         }
-
-        mDaysOfWeekStates[index] = state;
-
-        mSettingsManager.setDaysOfWeek(mDaysOfWeekStates);
     }
 
     @Override
-    public void setReminder1State(boolean enabled) {
-        mReminder1State = enabled;
+    public void onDeleteAllEntriesConfirmed() {
+        mEntryRepository.deleteAll();
+
+        if (mSettingsView != null) {
+            mSettingsView.displayDeletionDoneMessage();
+        }
     }
 
     @Override
-    public void setReminder1Time(String time) {
-        mReminder1Time = time;
-    }
-
-    @Override
-    public void setReminder2State(boolean enabled) {
-        mReminder2State = enabled;
-    }
-
-    @Override
-    public void setReminder2Time(String time) {
-        mReminder2Time = time;
-    }
-
-    @Override
-    public void setReminder3State(boolean enabled) {
-        mReminder3State = enabled;
-    }
-
-    @Override
-    public void setReminder3Time(String time) {
-        mReminder3Time = time;
-    }
-
-    @Override
-    public void onEditReminder1TimeClicked() {
-        String[] timeUnits = mReminder1Time.split(":");
-    }
-
-    @Override
-    public void onEditReminder2TimeClicked() {
-        String[] timeUnits = mReminder2Time.split(":");
-    }
-
-    @Override
-    public void onEditReminder3TimeClicked() {
-        String[] timeUnits = mReminder3Time.split(":");
+    public void onEntryLifespanChanged(int lifespanInDays) {
+        mSettingsManager.setEntryLifespan(lifespanInDays);
     }
 }
